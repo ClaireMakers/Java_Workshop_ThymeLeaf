ad8f2cfec6a7e32b20596b1c124328074448d799
lottec <lotte.cutts@gmail.com>
2025-11-24 16:21:17 +0000

    move controller dir to correct package

--- src/main/java/com/makers/makersbnb_coach_support_code/controller/PlanetsController.java
+++ src/main/java/com/makers/makersbnb_coach_support_code/controller/PlanetsController.java
@@ -1,4 +1,4 @@
-package com.makers.makersbnb_coach_support_code;
+package com.makers.makersbnb_coach_support_code.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
